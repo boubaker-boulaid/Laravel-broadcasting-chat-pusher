d0885cc1c49bdf98c91530408b9f522fbb117cce
boubaker <boubakerboulaid205@gmail.com>
2026-04-07 17:54:17 +0100

    feat: configure message routes and define fillable attributes in Message model

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\MessageContoller;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[MessageContoller::class, 'index']);
+Route::post('/send',[MessageContoller::class, 'send']);
