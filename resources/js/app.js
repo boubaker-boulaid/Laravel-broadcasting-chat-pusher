0cdcca2af86cf158385e781607b1a7815ddabdfc
boubaker <boubakerboulaid205@gmail.com>
2026-04-07 18:04:15 +0100

    feat: listen for MessageSentEvent on chat channel and append messages to DOM

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
-import './bootstrap';
+import "./bootstrap";
+window.Echo.channel("chat").listen("MessageSentEvent", (e) => {
+    console.log('message recieved :', e);
+    let div = document.getElementById("messages");
+    div.innerHTML += `<p>${e.message}</p>`;
+});
